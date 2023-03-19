e3ab88a5b9b2d0e07c3e4e2accbd99db495cf10c
Piero <pierojacinto@gmail.com>
2023-03-19 13:45:05 -0300

    head added, google fonts, fonts awesome and css linked, header mobile started

--- site/src/app.js
+++ site/src/app.js
@@ -2,6 +2,8 @@ require("dotenv").config({path:"../.env"});
 const express = require("express")
 const createApp = require("./config/create-app");
 const appConfig = require("./config/app-config");
+const { join } = require("path");
+const { static } = require("express");
 
 
 // CREATE EXPRESS APP
@@ -9,12 +11,18 @@ const app = createApp();
 
 appConfig.config(app);
 
+// public path
+
+app.use(static(join(__dirname, "..", "public")));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
 
 const mainRouter = require("./routers/main-router");
 
 // MOUNT MAIN ROUTER
 
-app.use(mainRouter);
+app.use(mainRouter);    
 
