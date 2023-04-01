7216fc41399a9a3244949e82e427207a04d287ca
Piero <pierojacinto@gmail.com>
2023-04-01 17:15:39 -0300

    instagram feed went back

--- site/src/app.js
+++ site/src/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config({path:"../.env"});
+require("dotenv").config();
 const express = require("express")
 const createApp = require("./config/create-app");
 const appConfig = require("./config/app-config");
