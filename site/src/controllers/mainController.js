afd9e57cfad01d852f3c197fd5034ce544a48441
Roni Graizman <ronigraiz@gmail.com>
2023-04-03 15:27:54 -0300

    fetch de instagram

--- site/src/controllers/mainController.js
+++ site/src/controllers/mainController.js
@@ -1,6 +1,15 @@
 
+// FETCH INSTAGRAM 
+const fetch = require('node-fetch');
+const token = process.env.IG_ACCESS_TOKEN
+const url = `https://graph.instagram.com/me/media?fields=thumbnail_url,media_url,caption,permalink&limit=5&access_token=${token}`;
+
 module.exports = {
     home: async (req, res) => {
-       res.render("home");
+       const instaFetch = await fetch(url)
+       const instaJson = await instaFetch.json()
+       const instaData = instaJson.data
+       console.log(instaJson);
+       res.render("home", { instaData });
    },
 };
