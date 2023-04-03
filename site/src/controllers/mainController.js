5434aeb0b49f2806354dc33d7785a76ca81e88ef
Piero <pierojacinto@gmail.com>
2023-04-03 18:28:33 -0300

    carrousel

--- site/src/controllers/mainController.js
+++ site/src/controllers/mainController.js
@@ -2,14 +2,13 @@
 // FETCH INSTAGRAM 
 const fetch = require('node-fetch');
 const token = process.env.IG_ACCESS_TOKEN
-const url = `https://graph.instagram.com/me/media?fields=thumbnail_url,media_url,caption,permalink&limit=5&access_token=${token}`;
+const url = `https://graph.instagram.com/me/media?fields=thumbnail_url,media_url,caption,permalink&limit=50&access_token=${token}`;
 
 module.exports = {
     home: async (req, res) => {
        const instaFetch = await fetch(url)
        const instaJson = await instaFetch.json()
-       const instaData = instaJson.data
-       console.log(instaJson);
+       const instaData = instaJson.data       
        res.render("home", { instaData });
    },
 };
