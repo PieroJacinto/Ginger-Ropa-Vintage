208e5b30173c4082a38919ae42d99ee36f14bfb3
Roni Graizman <ronigraiz@gmail.com>
2023-04-11 17:42:18 -0300

    creacion de base de datos, migrations, seeders

---
+++ site/src/database/models/UsuarioAdmin.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, dataTypes){
+
+    let alias = "UsuarioAdmin" //Como sequelize llama a nuestra tabla
+	let cols = {
+		id: {
+            type:dataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		nombre: {
+            type:dataTypes.STRING(500),
+            allowNull:false,
+        },
+        email: {
+            type: dataTypes.STRING(500),
+            allowNull:false,
+        },
+        contraseña:{
+            type:dataTypes.STRING(500),
+            allowNull:false,
+        },
+	}
+	let config = {
+		tableName: "usuario_admin",
+		timestamps: true
+	}
+}
